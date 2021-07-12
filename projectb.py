146b4626945e9261e9073426601124ce251bbc64
Darkninjaeston <86796477+Darkninjaeston@users.noreply.github.com>
2021-07-12 23:27:15 +0530

    Add files via upload

---
+++ projectb.py
@@ -0,0 +1,17 @@
+list1 = [12, -7, 5, 64, -14]
+list2 = [12, 14, -95, 3]
+
+print("list1 ")
+for x in list1:
+    if x >= 0:
+       print(x, end = " ")
+
+if 64==64:
+    print(" ")
+print("list2 ")
+for y in list2:
+
+    if y >= 0:
+       print(     y, end = " ")
+if 3==3:
+    print("")
